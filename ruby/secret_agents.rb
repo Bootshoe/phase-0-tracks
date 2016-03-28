fc11d215189c9431af2c07961ab5d216b16f7e97
Craig Waterman <Crgwtrmn@gmail.com>
2016-03-28 11:44:59 -0500

    4.6 done

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,12 @@
+#encrypt
+
+puts "Type message"
+message=gets.chomp.to_s
+message_length=(message.length)-1
+
+p message.next[0..message_length]
+
+#dcrypt 
+
+p message.next [0..-message_length]
+
