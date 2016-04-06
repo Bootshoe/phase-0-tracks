61ce52049c4a0e743f259537cbbdaa507f21378f
Bootshoe <Crgwtrmn@gmail.com>
2016-04-06 14:05:15 -0500

    Update puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,34 @@
+class Puppy
+end
+
+Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+spot.class
+
+duchess == fido
+
+fido.instance_of?(Array)
+
+fido.instance_of?(Puppy)
+
+spot.play_dead
+
+str = String.new
+
+str.length
+
+str.empty?
+
+str += "huh."
+
+str = "some string"
+
+greeting = String.new("hello")
+
+greeting.length
+
+greeting.upcase
