64d563cb286ff6fdf12df7dbc7e71a5a7d0c551e
Bootshoe <Crgwtrmn@gmail.com>
2016-05-02 09:27:01 -0500

    6.3 finished

---
+++ ruby/santa_nh.rb
@@ -0,0 +1,46 @@
+class Santa
+
+	def initialize(sex, race)
+		puts "Initializing Santa instance..."
+		@gneder=sex
+		@ethnicity=race
+		@age=0
+		@raindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+
+	def speak
+		puts "ho ho ho! Haaaaapy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+
+end
+
+=begin
+nick=Santa.new
+
+nick.speak
+nick.eat_milk_and_cookies("chocolate chip")
+=end
+
+=begin
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+=end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
